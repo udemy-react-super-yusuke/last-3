fe7615534c03cd9d5ef7aacd1e5defc8386a8b28
udemy-react-super-yusuke <38956215+udemy-react-super-yusuke@users.noreply.github.com>
2018-05-04 01:44:29 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# last-3
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import ImageList from "./containers/ImageList";
+
+const App = () => {
+  return (
+    <div>
+      App
+      <ImageList />
+    </div>
+  );
+};
+
+export default App;
